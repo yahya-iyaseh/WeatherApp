66582c137ba9b624910f290b578b55e04fa7782b
yahya-iyaseh <57312677+yahya-iyaseh@users.noreply.github.com>
2021-02-06 22:35:49 +0200

    final

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,13 @@
-// to get the form with form tag because we have one form in project
 const form = document.querySelector('form');
-// to get the card div with card class 
 const card = document.querySelector('.card');
-// to get the details div with details class
 const details = document.querySelector('.details');
-// to get the img(for day and night)
 const time = document.querySelector('img.time');
-// to get the img(for icon)
 const icon = document.querySelector('.icon img');
 
-//for update the form details and data  
+const forecast = new Forecast();
 const updateUi = (data) =>{
- //to get the data for city details and weather 
-   // const cityDets = data.cityDetalis;
-   // const weather = data.weather;
 
    const {cityDetalis, weather} = data;
-   // update the template
    details.innerHTML = `
      <h5 class="my-3">${cityDetalis.EnglishName}</h5>
    <div class="my-3">${weather.WeatherText}</div>
@@ -26,19 +17,11 @@ const updateUi = (data) =>{
       </div>
     </div>
    `; 
-   // set the icon based on the WeatherIcon from the api 
    const imgsrc = `icons/${weather.WeatherIcon}.svg`;
    let timeScr = null;
    
  
-   // if(weather.IsDayTime){
-   //   timeScr = 'icons/day.svg';
-   // } else{
-   //  timeScr= 'icons/night.svg';
-   // }
-   // decide the img(day or night) based in IsDayTime from the api
     timeScr = weather.IsDayTime ?  'icons/day.svg' : 'icons/night.svg';
-// set the imgs for the img(src) in the form
    icon.setAttribute('src', imgsrc);
    time.setAttribute('src', timeScr);
 
@@ -49,40 +32,21 @@ const updateUi = (data) =>{
    
 }
 
-// to call finction getCity and getWeather from forecast.js to get the data about the city that we pass her name 
-const updateCity = async city =>{
 
-    const cityDetalis = await getCity(city);
-    const weather = await getWeather(cityDetalis.Key);
-    
-   return {cityDetalis, weather};
- };
-// add eventListner to the form when we submit the form 
 form.addEventListener('submit', e => {
- // let us in the same page when we submit
  e.preventDefault();
 
- // to get city name and trim any space from firt and last of the string
  const city = form.city.value.trim();
 
- // to reset the form input (delete the value that we put it in the input)
  form.reset();
- //call updateCity and passes the name that we take it from the input 
- updateCity(city)
- .then(data => {
-// call the updateUi function that responsible to replace the form data and details
-  updateUi(data);
-  
- })
- .catch(err => console.log(err));
+ forecast.updateCity(city)
+    .then(data => updateUi(data))
+    .catch(err => console.log(err));
 
-  // store city in local Storage
   localStorage.setItem('city',city);
 });
-// if we have a city in the local Storage we update the page without 
-// have user to enter city names
 if(localStorage.getItem('city')){
-  updateCity(localStorage.getItem('city'))
+  forecast.updateCity(localStorage.getItem('city'))
   .then(data => updateUi(data))
   .catch(err => console.log(err));
 }
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,26 +1,29 @@
 // the link for the weather api from (accuweather)
-const key = 	'MT9UnClOvUx44A0NjpFPmWTblXF7ZLrS';
 
-// get weather info 
-    const getWeather = async  id =>{
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(base+query);
-    const data = await response.json();
-  return data[0];
+class Forecast{
+  constructor(){
+    this.key = 'MT9UnClOvUx44A0NjpFPmWTblXF7ZLrS';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+    const cityDetalis = await this.getCity(city);
+    const weather = await this.getWeather(cityDetalis.Key);
+   return {cityDetalis, weather};
+ }
+ async getCity(city){
+  const query = `?apikey=${this.key}&q=${city}`;
+  const response = await fetch(this.cityURI+query);
+  const data = await response.json();
+return data[0];
 };
-
-// get city info
-const getCity = async (city) =>{
-
-   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-   const query = `?apikey=${key}&q=${city}`;
-
-   const response = await fetch(base+query);
-   const data = await response.json();
- return data[0];
+async getWeather(id){  
+  const query = `${id}?apikey=${this.key}`;
+  const response = await fetch(this.weatherURI+query);
+  const data = await response.json();
+return data[0];
 };
+}
 
 
 
